12ea42bfb41b3ab2eb18ee342a5bef04d95aa35f
Fernando Mayer <fernandomayer@gmail.com>
2014-12-11 13:22:04 -0200

    atualizacao com correcoes

--- modulo_III/R_Modulo_III.R
+++ modulo_III/R_Modulo_III.R
@@ -1,20 +1,22 @@
 
-## @knitr setup
+## ----setup, include=FALSE, cache=FALSE-----------------------------------
 # smaller font size for chunks
 opts_chunk$set(size = "small",
-               prompt = TRUE,
+               prompt = FALSE,
                comment = NA,
                tidy = FALSE,
                cache = TRUE,
                fig.align = "center",
                fig.width = 5,
                fig.height = 5)
-thm <- knit_theme$get("beamer2")
-knit_theme$set(thm)
-options(width = 65, digits = 5, continue = "  ")
+## thm <- knit_theme$get("beamer3")
+## knit_theme$set(thm)
+options(width = 65, digits = 7, scipen = 0) # continue = "  ")
+## Use pdfcrop=TRUE nos chunks para reduzir a area do PDF
+knit_hooks$set(pdfcrop = hook_pdfcrop)
 
 
-## @knitr unnamed-chunk-1
+## ------------------------------------------------------------------------
 # valores críticos de z com alfa = 0,05 (bilateral)
 qnorm(0.025)
 qnorm(0.975)
@@ -23,23 +25,208 @@ qt(0.025, df = 9)
 qt(0.025,df = 900)
 
 
-## @knitr unnamed-chunk-2
-dados <- read.table("crabs.csv", header = T, sep = ";",
-                    dec = ",")
+## ------------------------------------------------------------------------
+## Dados
+xbarra <- 83
+desvio <- 12
+n <- 5
+## Erro padrão
+erro <- desvio/sqrt(n)
+## Média - erro
+xbarra + erro * qt(0.025, df = n)
+## Média + erro
+xbarra + erro * qt(0.975, df = n)
+
+
+## ------------------------------------------------------------------------
+dbinom(x = 0:1, size = 1, prob = .2)
+dbinom(x = 0:1, size = 1, prob = .5)
+dbinom(x = 0:1, size = 1, prob = .7)
+dbinom(x = 0:1, size = 1, prob = .9)
+
+
+## ----eval=FALSE, pdfcrop=TRUE--------------------------------------------
+## par(mfrow=c(2,2))
+## plot(0:1, dbinom(x = 0:1, size = 1, prob = .2), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "p = 0.2",
+##      ylim = c(0,1))
+## plot(0:1, dbinom(x = 0:1, size = 1, prob = .5), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "p = 0.5",
+##      ylim = c(0,1))
+## plot(0:1, dbinom(x = 0:1, size = 1, prob = .7), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "p = 0.7",
+##      ylim = c(0,1))
+## plot(0:1, dbinom(x = 0:1, size = 1, prob = .9), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "p = 0.9",
+##      ylim = c(0,1))
+
+
+## ----echo=FALSE, pdfcrop=TRUE, fig.width=8, fig.height=6-----------------
+par(mfrow=c(2,2))
+plot(0:1, dbinom(x = 0:1, size = 1, prob = .2), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "p = 0.2",
+     ylim = c(0,1))
+plot(0:1, dbinom(x = 0:1, size = 1, prob = .5), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "p = 0.5",
+     ylim = c(0,1))
+plot(0:1, dbinom(x = 0:1, size = 1, prob = .7), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "p = 0.7",
+     ylim = c(0,1))
+plot(0:1, dbinom(x = 0:1, size = 1, prob = .9), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "p = 0.9",
+     ylim = c(0,1))
+
+
+## ------------------------------------------------------------------------
+dbinom(x = 0:5, size = 5, prob = .2)
+dbinom(x = 0:5, size = 5, prob = .5)
+dbinom(x = 0:5, size = 5, prob = .7)
+dbinom(x = 0:5, size = 5, prob = .9)
+
+
+## ----eval=FALSE, pdfcrop=TRUE--------------------------------------------
+## par(mfrow=c(2,2))
+## plot(0:10, dbinom(x = 0:10, size = 10, prob = .2), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.2",
+##      ylim = c(0,.5))
+## plot(0:10, dbinom(x = 0:10, size = 10, prob = .5), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.5",
+##      ylim = c(0,.5))
+## plot(0:10, dbinom(x = 0:10, size = 10, prob = .7), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.7",
+##      ylim = c(0,.5))
+## plot(0:10, dbinom(x = 0:10, size = 10, prob = .9), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.9",
+##      ylim = c(0,.5))
+
+
+## ----echo=FALSE, pdfcrop=TRUE, fig.width=8, fig.height=6-----------------
+par(mfrow=c(2,2))
+plot(0:10, dbinom(x = 0:10, size = 10, prob = .2), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.2",
+     ylim = c(0,.5))
+plot(0:10, dbinom(x = 0:10, size = 10, prob = .5), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.5",
+     ylim = c(0,.5))
+plot(0:10, dbinom(x = 0:10, size = 10, prob = .7), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.7",
+     ylim = c(0,.5))
+plot(0:10, dbinom(x = 0:10, size = 10, prob = .9), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = "n = 10, p = 0.9",
+     ylim = c(0,.5))
+
+
+## ------------------------------------------------------------------------
+dpois(x = 0:10, lambda = 1)
+dpois(x = 0:10, lambda = 5)
+dpois(x = 0:10, lambda = 10)
+dpois(x = 0:10, lambda = 15)
+
+
+## ----eval=FALSE, pdfcrop=TRUE--------------------------------------------
+## par(mfrow=c(2,2))
+## plot(0:30, dpois(x = 0:30, lambda = 1), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = expression(mu == 1),
+##      ylim = c(0,.4))
+## plot(0:30, dpois(x = 0:30, lambda = 5), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = expression(mu == 5),
+##      ylim = c(0,.4))
+## plot(0:30, dpois(x = 0:30, lambda = 10), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = expression(mu == 10),
+##      ylim = c(0,.4))
+## plot(0:30, dpois(x = 0:30, lambda = 15), type = "h",
+##      xlab = "X", ylab = "P[X = x]", main = expression(mu == 15),
+##      ylim = c(0,.4))
+
+
+## ----echo=FALSE, pdfcrop=TRUE, fig.width=8, fig.height=6-----------------
+par(mfrow=c(2,2))
+plot(0:30, dpois(x = 0:30, lambda = 1), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = expression(mu == 1),
+     ylim = c(0,.4))
+plot(0:30, dpois(x = 0:30, lambda = 5), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = expression(mu == 5),
+     ylim = c(0,.4))
+plot(0:30, dpois(x = 0:30, lambda = 10), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = expression(mu == 10),
+     ylim = c(0,.4))
+plot(0:30, dpois(x = 0:30, lambda = 15), type = "h",
+     xlab = "X", ylab = "P[X = x]", main = expression(mu == 15),
+     ylim = c(0,.4))
+
+
+## ----size="footnotesize"-------------------------------------------------
+dnorm(x = 40:60, mean = 50, sd = 5)
+dnorm(x = 40:60, mean = 50, sd = 10)
+dnorm(x = 90:110, mean = 100, sd = 5)
+dnorm(x = 190:210, mean = 200, sd = 5)
+
+
+## ----eval=FALSE, pdfcrop=TRUE, size="footnotesize"-----------------------
+## par(mfrow=c(2,2))
+## plot(seq(10, 90, length=100), type = "l", xlab = "X", ylab = "f(x)",
+##      y = dnorm(x = seq(10, 90, length=100), mean = 50, sd = 5),
+##      main = expression(list(mu == 50, sigma^2 == 25)))
+## plot(seq(10, 90, length=100), type = "l", xlab = "X", ylab = "f(x)",
+##      y = dnorm(x = seq(10, 90, length=100), mean = 50, sd = 10),
+##      main = expression(list(mu == 50, sigma^2 == 100)))
+## plot(seq(70, 130, length=100), type = "l", xlab = "X", ylab = "f(x)",
+##      y = dnorm(x = seq(70, 130, length=100), mean = 100, sd = 5),
+##      main = expression(list(mu == 100, sigma^2 == 25)))
+## plot(seq(170, 230, length=100), type = "l", xlab = "X", ylab = "f(x)",
+##      y = dnorm(x = seq(170, 230, length=100), mean = 200, sd = 5),
+##      main = expression(list(mu == 200, sigma^2 == 25)))
+## par(mfrow=c(1,1))
+
+
+## ----echo=FALSE, pdfcrop=TRUE, fig.width=8, fig.height=6-----------------
+par(mfrow=c(2,2))
+plot(seq(10, 90, length=100), type = "l", xlab = "X", ylab = "f(x)",
+     y = dnorm(x = seq(10, 90, length=100), mean = 50, sd = 5),
+     main = expression(list(mu == 50, sigma^2 == 25)))
+plot(seq(10, 90, length=100), type = "l", xlab = "X", ylab = "f(x)",
+     y = dnorm(x = seq(10, 90, length=100), mean = 50, sd = 10),
+     main = expression(list(mu == 50, sigma^2 == 100)))
+plot(seq(70, 130, length=100), type = "l", xlab = "X", ylab = "f(x)",
+     y = dnorm(x = seq(70, 130, length=100), mean = 100, sd = 5),
+     main = expression(list(mu == 100, sigma^2 == 25)))
+plot(seq(170, 230, length=100), type = "l", xlab = "X", ylab = "f(x)",
+     y = dnorm(x = seq(170, 230, length=100), mean = 200, sd = 5),
+     main = expression(list(mu == 200, sigma^2 == 25)))
+par(mfrow=c(1,1))
+
+
+## ------------------------------------------------------------------------
+dados <- read.table("../dados/crabs.csv", header = T,
+                    sep = ";", dec = ",")
 str(dados)
 
 
-## @knitr unnamed-chunk-3
+## ----out.width=".6\\textwidth"-------------------------------------------
 hist(dados$CL, main = "", ylab = "Frequência absoluta",
      xlab = "Comprimento da carapaça (mm)", col = "grey")
 
 
-## @knitr unnamed-chunk-4
+## ------------------------------------------------------------------------
 t.test(dados$CL, mu = 30, alternative = "two.sided",
        conf.level = 0.95)
 
 
-## @knitr unnamed-chunk-5
+## ------------------------------------------------------------------------
+## Dados
+xbarra <- mean(dados$CL)
+mu0 <- 30
+dp <- sd(dados$CL)
+n <- nrow(dados)
+# t calculado
+(tcalc <- (xbarra - mu0)/(dp/sqrt(n)))
+# t critico (não é apresentado no resultado)
+qt(0.025, df = n - 1, lower.tail = FALSE)
+# valor p (multiplicado por 2 pois o teste é bilateral)
+pt(tcalc, df = n - 1, lower.tail = FALSE) * 2
+
+
+## ------------------------------------------------------------------------
 teste <- t.test(dados$CL, mu = 30, alternative = "two.sided",
                 conf.level = 0.95)
 names(teste)
@@ -47,91 +234,174 @@ teste$statistic
 teste$p.value
 
 
-## @knitr unnamed-chunk-6
+## ------------------------------------------------------------------------
 t.test(dados$CL, mu = 30, alternative = "greater",
        conf.level = 0.95)
 
 
-## @knitr unnamed-chunk-7
+## ------------------------------------------------------------------------
 t.test(dados$CL, mu = 30, alternative = "less",
        conf.level = 0.95)
 
 
-## @knitr unnamed-chunk-8
+## ----eval=FALSE, echo=FALSE, out.width=".6\\textwidth"-------------------
+## par(mfrow = c(1,2))
+## hist(dados$CL[dados$especie == "azul"], main = "Azul",
+##      xlab = "CL (mm)", ylab = "Frequência", xlim = c(10, 50))
+## hist(dados$CL[dados$especie == "laranja"], main = "Laranja",
+##      xlab = "CL (mm)", ylab = "Frequência", xlim = c(10, 50))
+## par(mfrow = c(1,1))
+
+
+## ----out.width=".6\\textwidth", fig.width=6, fig.height=5, fig.show="hold"----
+par(mfrow = c(1,2))
+hist(dados$CL[dados$especie == "azul"], main = "Azul",
+     xlab = "CL (mm)", ylab = "Frequência", xlim = c(10, 50))
+hist(dados$CL[dados$especie == "laranja"], main = "Laranja",
+     xlab = "CL (mm)", ylab = "Frequência", xlim = c(10, 50))
+par(mfrow = c(1,1))
+
+
+## ----out.width=".6\\textwidth"-------------------------------------------
 require(lattice) # pacote para gráficos avançados
 histogram(~CL | especie, data = dados)
 
 
-## @knitr unnamed-chunk-9
+## ------------------------------------------------------------------------
 with(dados, tapply(CL, especie, summary))
 
 
-## @knitr unnamed-chunk-10
+## ------------------------------------------------------------------------
 t.test(CL ~ especie, data = dados, mu = 0,
        alternative = "two.sided", conf.level = 0.95)
 
 
-## @knitr unnamed-chunk-11
+## ------------------------------------------------------------------------
 t.test(CL ~ especie, data = dados, mu = 0,
        alternative = "greater", conf.level = 0.95)
 
 
-## @knitr unnamed-chunk-12
+## ----out.width=".6\\textwidth"-------------------------------------------
 plot(CW ~ CL, data = dados)
 
 
-## @knitr unnamed-chunk-13
-plot(1:10, 1:10, type = "l", xlab = "", ylab = "",  main = "r = 1")
-plot(1:10, rep(5,10), type = "l", xlab = "", ylab = "", main = "r = 0")
-plot(1:10, -1:-10, type = "l", xlab = "", ylab = "", main = "r = -1")
-
-
-## @knitr unnamed-chunk-14
-cor(dados$CL, dados$CW)
-cor.test(dados$CL, dados$CW)
-
-
-## @knitr unnamed-chunk-15
+## ------------------------------------------------------------------------
 mod <- lm(CW ~ CL, data = dados)
 mod
 
 
-## @knitr unnamed-chunk-16
+## ----size="footnotesize"-------------------------------------------------
 summary(mod)
 
 
-## @knitr unnamed-chunk-17
+## ------------------------------------------------------------------------
+anova(mod)
+
+
+## ----out.width=".49\\textwidth", fig.show="hold"-------------------------
 plot(CW ~ CL, data = dados)
 abline(mod)
 plot(CW ~ CL, data = dados, xlim = c(0,50), ylim = c(0,55))
 abline(mod)
 
 
-## @knitr unnamed-chunk-18
+## ----out.width=".6\\textwidth", fig.show="hold", fig.width=7, fig.height=7----
 par(mfrow = c(2,2))
 plot(mod)
 par(mfrow = c(1,1))
 
 
-## @knitr unnamed-chunk-19
+## ------------------------------------------------------------------------
 names(mod)
 names(summary(mod))
+names(anova(mod))
 
 
-## @knitr unnamed-chunk-20
+## ------------------------------------------------------------------------
+sqrt(anova(mod)$Sum[2]/anova(mod)$Df[2])
+
+
+## ------------------------------------------------------------------------
+anova(mod)$F[1]
+
+
+## ------------------------------------------------------------------------
+summary(mod)$coef[2,3]^2
+
+
+## ----out.width=".33\\textwidth",fig.show="hold", echo=FALSE,fig.width=5,fig.height=5----
+plot(1:10, 1:10, type = "l", xlab = "", ylab = "",  main = "r = 1")
+plot(1:10, rep(5,10), type = "l", xlab = "", ylab = "", main = "r = 0")
+plot(1:10, -1:-10, type = "l", xlab = "", ylab = "", main = "r = -1")
+
+
+## ----echo=FALSE,pdfcrop=TRUE, fig.width=8, fig.height=6------------------
+x <- (1:100)/10
+n <- length(x)
+set.seed(1000)
+par(mfrow=c(2,2))
+
+e <- rnorm(n, 0, 2)
+y <- 20 + x + e
+cor <- round(cor(x,y), 2)
+r <- round(cor^2, 2)
+lis <- list(bquote(r == .(cor)),
+            bquote(r^2 == .(r)))
+plot(y ~ x, xlim = c(0, 10), ylim = c(0, 45))
+mtext(do.call(expression, lis), side = 3, line = 0:1)
+mm <- lm(y ~ x)
+abline(mm, lwd = 2)
+
+e <- rnorm(n, 0, 4)
+y <- 20 + x + e
+cor <- round(cor(x,y), 2)
+r <- round(cor^2, 2)
+lis <- list(bquote(r == .(cor)),
+            bquote(r^2 == .(r)))
+plot(y ~ x, xlim = c(0, 10), ylim = c(0, 45))
+mtext(do.call(expression, lis), side = 3, line = 0:1)
+mm <- lm(y ~ x)
+abline(mm, lwd = 2)
+
+e <- rnorm(n, 0, 6)
+y <- 20 + x + e
+cor <- round(cor(x,y), 2)
+r <- round(cor^2, 2)
+lis <- list(bquote(r == .(cor)),
+            bquote(r^2 == .(r)))
+plot(y ~ x, xlim = c(0, 10), ylim = c(0, 45))
+mtext(do.call(expression, lis), side = 3, line = 0:1)
+mm <- lm(y ~ x)
+abline(mm, lwd = 2)
+
+e <- rnorm(n, 0, 8)
+y <- 20 + x + e
+cor <- round(cor(x,y), 2)
+r <- round(cor^2, 2)
+lis <- list(bquote(r == .(cor)),
+            bquote(r^2 == .(r)))
+plot(y ~ x, xlim = c(0, 10), ylim = c(0, 45))
+mtext(do.call(expression, lis), side = 3, line = 0:1)
+mm <- lm(y ~ x)
+abline(mm, lwd = 2)
+
+par(mfrow = c(1,1))
+
+
+## ------------------------------------------------------------------------
 with(dados, tapply(CL, especie, summary))
 
 
-## @knitr unnamed-chunk-21
+## ------------------------------------------------------------------------
 mean(dados$CL)
 
 
-## @knitr unnamed-chunk-22
+## ----out.width=".6\\textwidth"-------------------------------------------
 boxplot(CL ~ especie, data = dados)
 abline(h = mean(dados$CL), lty = 2, col = "red", lwd = 2)
 
 
-## @knitr unnamed-chunk-23
+## ----out.width=".7\\textwidth", echo=FALSE-------------------------------
 plot(CL ~ as.numeric(especie), data = dados, axes = FALSE,
      xlim = c(0,3), xlab = "Espécie", ylab = "CL")
 axis(1, at = seq(0,3,1), labels = c("", "Azul", "Laranja", ""), tick = FALSE)
@@ -143,12 +413,12 @@ points(2, mean(dados$CL[dados$especie == "laranja"]), pch = 15,
 abline(h = mean(dados$CL), lty = 2, col = "red", lwd = 2)
 
 
-## @knitr unnamed-chunk-24
+## ----size="footnotesize"-------------------------------------------------
 mod <- lm(CL ~ especie, data = dados)
 summary(mod)
 
 
-## @knitr unnamed-chunk-25
+## ----out.width=".7\\textwidth", echo=FALSE-------------------------------
 plot(CL ~ as.numeric(especie), data = dados, axes = FALSE,
      xlim = c(0,3), xlab = "Espécie", ylab = "CL")
 axis(1, at = seq(0,3,1), labels = c("", "Azul", "Laranja", ""), tick = FALSE)
@@ -163,13 +433,13 @@ segments(1, mean(dados$CL[dados$especie=="azul"]),
 # abline(mod)
 
 
-## @knitr unnamed-chunk-26
+## ----size="footnotesize"-------------------------------------------------
 teste <- t.test(CL ~ especie, data = dados, mu = 0,
                 alternative = "two.sided", conf.level = 0.95)
 teste
 
 
-## @knitr unnamed-chunk-27
+## ----size="footnotesize", echo=-c(1,2,7)---------------------------------
 sci <- getOption("scipen")
 options(scipen = -1)
 summary(mod)$coefficients
@@ -179,16 +449,16 @@ diff(teste$estimate)
 options(scipen = sci)
 
 
-## @knitr unnamed-chunk-28
+## ------------------------------------------------------------------------
 anova(mod)
 
 
-## @knitr unnamed-chunk-29
+## ------------------------------------------------------------------------
 mod.anova <- aov(CL ~ especie, data = dados)
 TukeyHSD(mod.anova)
 
 
-## @knitr unnamed-chunk-30
+## ----size="footnotesize"-------------------------------------------------
 mod.glm <- glm(CL ~ especie, data = dados,
                family = gaussian(link = "identity"))
 summary(mod.glm)
